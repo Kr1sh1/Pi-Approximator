e39abd3ee32d01390d31ee9ce412a2b24f133029
Krishi Wali <krishiwali@hotmail.co.uk>
2020-04-15 16:27:40 +0100

    Approximations are displayed every second. Added colours.
    Colors require colorama module.

--- Monte Carlo method of approximating pi.py
+++ Monte Carlo method of approximating pi.py
@@ -1,14 +1,27 @@
 from random import uniform
-from decimal import Decimal
-from decimal import getcontext
-from time import time
+from decimal import Decimal, getcontext
+from time import time, sleep
 from math import sqrt
+from threading import Thread
+from colorama import init, Fore
+
+n = 0
+m = 0
+
+def print_approximation():
+    init()
+    start_time = time()
+    getcontext().prec = 20
+    while True:
+        sleep(1)
+        approx_pi = (Decimal(m*4)/Decimal(n))
+        time_running = int(time() - start_time)
+        print(f"{Fore.GREEN}Approximate value of pi: {approx_pi}{Fore.BLUE} Number of total points: {n}"
+        f"{Fore.YELLOW} Number of points within first quadrant of the circle: {m}{Fore.RED} Time running: {time_running}s", end="\r")
+
 
 def main():
-    n = 0
-    m = 0
-    getcontext().prec = 100
-    start_time = time()
+    global n, m
 
     try:
         while True:
@@ -19,11 +32,8 @@ def main():
                 m += 1
         
     except KeyboardInterrupt:
-        approx_pi = (Decimal(m*4)/Decimal(n))
-        time_running = int(time() - start_time)
-        print(f"Approximate value of pi: {approx_pi}\nNumber of total points: {n}\n"
-        f"Number of points within first quadrant of the circle: {m}\nTime running: {time_running}s")
         return
 
 if __name__ == "__main__":
+    Thread(target=print_approximation, daemon=True).start()
     main()
--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Pi-Approximator
 Different Algorithms with which Pi can be estimated
-
-Run the program and press CTRL C (Keyboard Interrupt) when you want the result
